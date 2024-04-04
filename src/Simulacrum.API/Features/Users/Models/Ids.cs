8f150102957525f10017718789406af33a0d25f1
Elianora Rose <developer@elianorarose.com>
2024-04-04 19:59:05 -0400

    Adding Users feature

---
+++ src/Simulacrum.API/Features/Users/Models/Ids.cs
@@ -0,0 +1,9 @@
+using Vogen;
+
+namespace Simulacrum.API.Features.Users.Models;
+
+[ValueObject<string>]
+public readonly partial struct Auth0UserId { }
+
+[ValueObject<int>]
+public readonly partial struct UserId { }
